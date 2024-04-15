1b6264b16dcfefd20c6787ea2c496cdaad09cd06
DEV-E3DAM <139201770+Darksourcebot@users.noreply.github.com>
2024-04-15 05:07:30 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+sudo apt update && apt upgrade -y 
+sudo apt install python3-pip ffmpeg -y
+sudo curl -sL https://deb.nodesource.com/setup_17.x | sudo bash -
+sudo apt-get install -y nodejs
+sudo npm i -g npm
+git clone https://github.com/AMANTYA1/RaiChuMusic  #Clone your repo.
+cd RaiChu-MusicV2
+sudo pip3 install --upgrade pip
+sudo pip3 install -U -r requirements.txt
+sudo python3 main.py
